035d27aa48fead3698a8836f21ef67e7c2053f76
Phan Châu Hải Lâm <phanchauhailam15803@gmail.com>
2024-05-18 00:04:27 +0700

    use ViewPager2 to switch fragment
    dùng viewpager2 để chuyển đổi qua lại giữa 2 fragment

--- app/src/main/java/com/pchailam/noteapp/MainActivity.java
+++ app/src/main/java/com/pchailam/noteapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -13,9 +14,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity   {
+    ViewPager2 viewPager2;
+    ViewPager2Adapter viewPager2Adapter;
     BottomNavigationView bottomNavigationView;
-    NotesFragment notesFragment;
-    TypeFragment typeFragment;
+//    NotesFragment notesFragment;
+//    TypeFragment typeFragment;
 
     @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
@@ -23,39 +26,62 @@ public class MainActivity extends AppCompatActivity   {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        notesFragment = new NotesFragment();
-        typeFragment = new TypeFragment();
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,notesFragment).commit();
-        bottomNavigationView.setSelectedItemId(R.id.notes);
+
+        viewPager2 = findViewById(R.id.viewPager2);
+        viewPager2Adapter = new ViewPager2Adapter(this);
+        viewPager2.setAdapter(viewPager2Adapter);
+
+//        notesFragment = new NotesFragment();
+//        typeFragment = new TypeFragment();
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.setSelectedItemId(R.id.notes);
+                        break;
+                    case 1:
+                        bottomNavigationView.setSelectedItemId(R.id.types);
+                        break;
+                }
+            }
+        });
+
+//        getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,notesFragment).commit();
+//        bottomNavigationView.setSelectedItemId(R.id.notes);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
 
                 if(id == R.id.notes) {
-                    switchFragment(notesFragment,false);
+//                    switchFragment(notesFragment,false);
+                    viewPager2.setCurrentItem(0);
+                    return true;
                 } else if(id == R.id.types) {
-                    switchFragment(typeFragment,true);
+//                    switchFragment(typeFragment,true);
+                    viewPager2.setCurrentItem(1);
+                    return true;
                 } else
                     return false;
-                return true;
             }
         });
     }
-    private void switchFragment(Fragment fragment, boolean isLeftToRight) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        if (isLeftToRight) {
-            transaction.setCustomAnimations(R.anim.enter_left_to_right, R.anim.exit_right_to_left,
-                    R.anim.enter_right_to_left, R.anim.exit_right_to_left);
-        } else {
-            transaction.setCustomAnimations(R.anim.enter_right_to_left, R.anim.exit_left_to_right,
-                    R.anim.enter_left_to_right, R.anim.exit_left_to_right);
-        }
-        transaction.replace(R.id.flFragment, fragment)
-                .addToBackStack(null)
-                .commit();
-    }
+//    private void switchFragment(Fragment fragment, boolean isLeftToRight) {
+//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//        if (isLeftToRight) {
+//            transaction.setCustomAnimations(R.anim.enter_left_to_right, R.anim.exit_right_to_left,
+//                    R.anim.enter_right_to_left, R.anim.exit_right_to_left);
+//        } else {
+//            transaction.setCustomAnimations(R.anim.enter_right_to_left, R.anim.exit_left_to_right,
+//                    R.anim.enter_left_to_right, R.anim.exit_left_to_right);
+//        }
+//        transaction.replace(R.id.flFragment, fragment)
+//                .addToBackStack(null)
+//                .commit();
+//    }
 
 }
